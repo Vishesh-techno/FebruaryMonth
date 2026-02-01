432911d882cc1e70d151afd4c488fe35a6446909
Vishesh-techno <sonivishesh099@gmail.com>
2026-02-01 17:27:02 +0530

    Today's Code'

--- src/OneFebruary.java
+++ src/OneFebruary.java
@@ -1,5 +1,56 @@
-public class OneFebruary {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class OneFebruary {
+    public static boolean areAnagrams(String s1, String s2) {
+//         if s1 and s2 have same character with their character count return true;
+        if (s1.length() != s2.length()) return false;
+        int[] res = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            res[s1.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            res[s2.charAt(i) - 'a']--;
+        }
+
+        for (int re : res) {
+            if (re != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(String s) {
+        char[] ch = s.toCharArray();
+        int start = 0, end = ch.length - 1;
+        while (start <= end) {
+            if (ch[start] != ch[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The First String: ");
+        String s = sc.next();
+        System.out.print("Enter The Second String: ");
+        String t = sc.next();
+        if (areAnagrams(s, t)) {
+            System.out.println("The Strings are Anagrams");
+        } else {
+            System.out.println("The Strings are not Anagrams");
+        }
+        System.out.print("Enter The String: ");
+        String s1 = sc.next();
+        if (isPalindrome(s1)) {
+            System.out.println("The Strings is Palindrome");
+        } else {
+            System.out.println("The Strings is not a palindrome");
+        }
     }
 }
