9d1073d7b3097a8a2cfb310990c29fbcff4dc435
Vishesh-techno <sonivishesh099@gmail.com>
2026-02-06 13:52:18 +0530

    Today's Code

---
+++ src/SixFebruary.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+
+public class SixFebruary {
+    public static int longestSubArray(int[] arr, int k) {
+        int n = arr.length;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum == k) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    public static int longestSubArrayBetter(int[] arr, int k) {
+//        this code is valid only non negative integers Exist
+        int n = arr.length;
+        int maxLen = 0;
+        int l = 0, r = 0, sum = 0;
+        while (r < n - 1) {
+            sum = sum + arr[r];
+            while (sum > k) {
+                sum = sum - arr[l];
+                l++;
+            }
+            if (sum == k) {
+                maxLen = Math.max(maxLen, r - l + 1);
+            }
+            r++;
+        }
+        return maxLen;
+    }
+
+    public static int longestSubArrayOptimal(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = arr.length;
+        int maxLen = 0, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + arr[i];
+            if (sum == k) {
+                maxLen = i + 1;
+            }
+            if (map.containsKey(sum - k)) {
+                maxLen = Math.max(maxLen, i - map.get(sum - k));
+            }
+            map.putIfAbsent(sum, i);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 2, 7, 1, 10};
+        System.out.println(longestSubArray(arr, 15));
+        System.out.println(longestSubArrayBetter(arr, 15));
+    }
+}
