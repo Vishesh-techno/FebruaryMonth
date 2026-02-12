199663cfc7d6d46c27aba8f223331d83aeb5d3e9
Vishesh-techno <sonivishesh099@gmail.com>
2026-02-12 12:42:42 +0530

    Today's Practice Program

--- src/TwelveFebruary.java
+++ src/TwelveFebruary.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +25,33 @@ public class TwelveFebruary {
         return true;
     }
 
+    public static int removeElement(int[] nums, int val) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+        return idx;
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        if(nums.length <= 2) return nums.length;
+        int idx = 2;
+        for(int i = 2; i<nums.length; i++){
+            if(nums[i] != nums[idx-2]){
+                nums[idx++] = nums[i];
+            }
+        }
+        return idx;
+    }
+
     public static void main(String[] args) {
         System.out.println(isIsomorphicStrings("egg", "nee"));
+        int[] nums = {0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(nums, 2));
+        System.out.println(removeDuplicates(nums));
     }
 }
